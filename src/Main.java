1934414fcb0a9106534f2cbab8b1edd0622bb3d8
Svetlana Andros <lana-444@mail.ru>
2024-03-29 00:14:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b);
+
+        calc.println.accept(c);
+
+    }
+}
+
+
+// Исправление дз, с помощью тернарного опретаора проверяем ноль в знаменателе ,
+// если есть - возвращаем ноль, если нет - результат производим деление
